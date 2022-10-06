e71274da64d6c339676083369bbc2f7fe5cb4814
MasihMuhammadi <masihmuhammadi202@gmail.com>
2022-10-07 03:14:38 +0430

    new feature

--- src/animation.jsx
+++ src/animation.jsx
@@ -1,7 +1,7 @@
 import react,{useEffect,useState} from 'react';
 
 const Animation = () =>{
-    
+
     const[build,setBuild] = useState(<span className="build">Build Your Own <br /><span className="bigResume">Resume</span></span>);
     const [choose,setChoose] = useState(<span className="choose">Or Choose Your <br /><span className="bigResume">Resume</span ></span>)
     const [chooseOrBuild,setChooseOrBuild] = useState(<span className="buildOrchoose">Build Or Choose Your <br /> <span className="bigResume">Resume</span></span>)
@@ -21,7 +21,7 @@ const Animation = () =>{
     
     return(<>
     <div className='wallpaper'>
-        <div className="getStarted">get Started</div>
+        
     <h4 className='bio'>{build}</h4>
     </div>
     </>)
