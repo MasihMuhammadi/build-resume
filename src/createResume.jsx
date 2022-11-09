9a91927fa731677f91ad8016c1d37ac00659d489
MasihMuhammadi <masihmuhammadi202@gmail.com>
2022-11-10 00:05:19 +0430

    login validation and handle some styles

--- src/createResume.jsx
+++ src/createResume.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 
 const CreatResume = () =>{
 return<>
-      <div className='creatResume bg-dark'>
-        <div>
-          
-            <input type="text" />
-            <input type="text" />
-         
-        </div>
-      </div>
+    <div className='text-dark'> Copy Your linkedin URL </div> <br />
+  <span className="alert text-dark alert-primary">comming soon...</span>
     </>
 }
 export default CreatResume;
